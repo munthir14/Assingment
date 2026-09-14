83daae12e64af693ee8067dea2cfbc0773abde85
MunthirAlbakous <munther.al2005@gmail.com>
2026-09-15 00:13:08 +0200

    2st answer

--- assingment_session05/Program.cs
+++ assingment_session05/Program.cs
@@ -8,6 +8,17 @@
             //Q1
             //double[] hi = [25.5, 40.0, 33.75];
             //Console.WriteLine(hi[1]);
+
+
+
+            //Q2
+            //int[,] shelfCopies = new int[2, 2]
+            //{
+
+            //    {3,5 },
+            //    {1,4 }
+            //};
+            //Console.WriteLine(shelfCopies[1, 0]);
         }
     }
 }
