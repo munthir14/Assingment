965ffc943cd27030a3bd0fbcb58a8405d90809fb
MunthirAlbakous <munther.al2005@gmail.com>
2026-09-14 23:59:17 +0200

    Add project files.

---
+++ assingment_session05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assingment_session05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
