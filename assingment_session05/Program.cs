98df97cb224a34147b64ac18a46cfa57de3cc22a
MunthirAlbakous <munther.al2005@gmail.com>
2026-09-15 00:06:48 +0200

    1st answer

--- assingment_session05/Program.cs
+++ assingment_session05/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Q1
+            //double[] hi = [25.5, 40.0, 33.75];
+            //Console.WriteLine(hi[1]);
         }
     }
 }
